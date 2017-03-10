2ef3a2d179e039a84b921217ce9e2e216687f61c
Florian Thomas <flthomas@ares.Physik.Uni-Mainz.DE>
2017-03-10 19:19:02 +0100

    update

--- build/tests/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /localscratch/flthomas/Wordom/tests
-# Build directory: /localscratch/flthomas/Wordom/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
